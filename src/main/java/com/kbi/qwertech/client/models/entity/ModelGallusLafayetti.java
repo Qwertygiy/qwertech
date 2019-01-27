f3011d9c9582966fff5247aeff41a66044efce96
Qwertygiy <qwertygiy@gmail.com>
2019-01-27 17:21:20 -0500

    More adjustments to Sri Lankan Junglefowl
    Adjusts the texture to be colorized closer to the intended design, as well as fixes some errors in the model.

--- src/main/java/com/kbi/qwertech/client/models/entity/ModelGallusLafayetti.java
+++ src/main/java/com/kbi/qwertech/client/models/entity/ModelGallusLafayetti.java
@@ -1,6 +1,6 @@
 package com.kbi.qwertech.client.models.entity;
 
-import com.kbi.qwertech.api.client.models.ModelRendererDefaults;
+import com.kbi.qwertech.api.client.registry.AnimationHelper;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
@@ -8,7 +8,7 @@ import net.minecraft.entity.Entity;
  * ModelGallusLafayetti - Qwertygiy
  * Created using Tabula 4.1.1
  */
-public class ModelGallusLafayetti extends ModelGallusGallusJabouille {
+public class ModelGallusLafayetti extends ModelWildChicken {
 
     public ModelGallusLafayetti() {
         super();
@@ -16,16 +16,26 @@ public class ModelGallusLafayetti extends ModelGallusGallusJabouille {
         this.textureHeight = 32;
         this.setRotateAngle(tail, 1.9198621771937625F, 0.0F, 0.0F);
         this.setRotateAngle(tail2, -0.3490658503988659F, 0.0F, 0.0F);
-        this.wattle = new ModelRendererDefaults(this, 18, 9);
+        this.setRotateAngle(neck, 0.3490658503988659F, 0.0F, 0.0F);
+        this.setRotateAngle(head, -0.3490658503988659F, 0.0F, 0.0F);
+
+        this.wattle.setTextureOffset(18, 9);
         this.wattle.setRotationPoint(0.0F, 1.7F, -1.0F);
+        this.wattle.cubeList.clear();
         this.wattle.addBox(-0.5F, 0.0F, -1.0F, 1, 2, 2, 0.0F);
         this.setRotateAngle(wattle, -0.3490658503988659F, 0.0F, 0.0F);
-        this.crest = new ModelRendererDefaults(this, 17, 19);
+
+        this.crest.setTextureOffset(17, 19);
         this.crest.setRotationPoint(0.0F, -0.6F, -0.5F);
+        this.crest.cubeList.clear();
         this.crest.addBox(-0.5F, -2.0F, -2.0F, 1, 2, 5, 0.0F);
         this.setRotateAngle(crest, 0.7853981633974483F, 0.0F, 0.0F);
-        this.head.addChild(this.wattle);
+
+        this.head.childModels.clear();
         this.head.addChild(this.crest);
+        this.head.addChild(this.wattle);
+        this.head.addChild(this.beak);
+        AnimationHelper.setAsDefault(this);
     }
 
     @Override
